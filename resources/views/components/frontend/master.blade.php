32392d3dab5269a1bb668b12b898f96135c9f0c0
EugeniaDzadu <jeeniaace@gmail.com>
2024-08-20 11:26:49 +0000

    update: footer added for user

--- resources/views/components/frontend/master.blade.php
+++ resources/views/components/frontend/master.blade.php
@@ -424,7 +424,130 @@
         </div>
     </header>
 
+    
 
+
+     <!-- footer call to action area start -->
+     <div class="footer-callto-action-area bg-light-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="call-to-sction bg_image shape-move">
+                        <h2 class="title">Skills Certificate From <br> the Studyhub</h2>
+                        <a href="course-one.html" class="rts-btn btn-primary-white with-arrow">View All Course <i class="fa-regular fa-arrow-right"></i></a>
+                        <div class="cta-image">
+                            <img src="images/women.png" alt="">
+                        </div>
+                        <div class="shape-image">
+                            <div class="shape one" data-speed="0.04"><img src="images/03_2.svg" alt=""></div>
+                            <div class="shape two" data-speed="0.04"><img src="images/04_2.svg" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row  ptb--100">
+                <div class="col-lg-12">
+                    <!-- footer main wrapper -->
+                    <div class="footer-one-main-wrapper">
+                        <!-- single sized  footer  -->
+                        <div class="footer-singl-wized left-logo">
+                            <div class="head">
+                                <a href="#">
+                                    <img src="{{asset('assets/images/logo-1.svg')}}" alt="logo" loading="lazy">
+                                </a>
+                            </div>
+                            <div class="body">
+                                <p class="dsic">
+                                    We are passionate education dedicated to providing high-quality resources learners
+                                    all backgrounds.
+                                </p>
+                                <ul class="wrapper-list">
+                                    <li><i class="fa-regular fa-location-dot"></i>Yarra Park, Melbourne, Australia </li>
+                                    <li><i class="fa-regular fa-phone"></i><a href="tel:+4733378901">+(61) 485-826-710</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- single sized  footer end -->
+                        <!-- single sized  footer  -->
+                        <div class="footer-singl-wized">
+                            <div class="head">
+                                <h6 class="title">Quick Links</h6>
+                            </div>
+                            <div class="body">
+                                <ul class="menu">
+                                    <li><a href="course-two.html">Latest Courses</a></li>
+                                    <li><a href="about.html">Mission &amp; Vision</a></li>
+                                    <li><a href="become-instructor.html">Join a Carrer</a></li>
+                                    <li><a href="zoom-meeting.html">Zoom Meeting</a></li>
+                                    <li><a href="pricing.html">Pricing Plan</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- single sized  footer end -->
+                        <!-- single sized  footer  -->
+                        <div class="footer-singl-wized">
+                            <div class="head">
+                                <h6 class="title">Explore</h6>
+                            </div>
+                            <div class="body">
+                                <ul class="menu">
+                                    <li><a href="course-one.html">Course One</a></li>
+                                    <li><a href="course-two.html">Course Two</a></li>
+                                    <li><a href="create-course.html">Create Course</a></li>
+                                    <li><a href="lesson-details.html">Lesson Details</a></li>
+                                    <li><a href="instructor.html">Instructor</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- single sized  footer end -->
+                        <!-- single sized  footer  -->
+                        <div class="footer-singl-wized input-area">
+                            <div class="head">
+                                <h6 class="title">Newsletter</h6>
+                            </div>
+                            <div class="body">
+                                <p class="disc">Subscribe Our newsletter get update our new course</p>
+                                <form action="#">
+                                    <div class="input-area-fill">
+                                        <input type="email" placeholder="Enter Your Email" required="">
+                                        <button> Subscribe</button>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <input type="checkbox" id="exampleCheck1">
+                                        <label for="exampleCheck1">I agree to the terms of use and privacy policy.</label>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- single sized  footer end -->
+                    </div>
+                    <!-- footer main wrapper end -->
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area-one-border">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="copyright-area-one">
+                            <p>Copyright © 2024 All Rights Reserved by Studyhub</p>
+                            <div class="social-copyright">
+                                <ul>
+                                    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
      <!-- jquery min js -->
