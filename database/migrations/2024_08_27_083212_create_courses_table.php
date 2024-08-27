3059b94045713d0545aeab4be64f437b698d80c0
oseijoel6111 <oseijoel6111@gmail.com>
2024-08-26 20:55:02 -1200

    update: students,course,registration,session, program tables added

---
+++ database/migrations/2024_08_27_083212_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_code');
+            $table->string('course_name');
+            $table->foreignId('program_id')->constrained('programs');
+            $table->integer('credits');
+            $table->string('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
